00f7b2863a74aa0ce4a8c02a1db525c5cf093c93
Silvia Neves <siltatiana.sn@gmail.com>
2022-08-25 14:35:21 +0100

    adds validations

---
+++ app/models/list.rb
@@ -0,0 +1,8 @@
+class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+
+  validates :name, uniqueness: true
+  validates :name, presence: true
+
+end
---
+++ db/migrate/20220825125703_add_movie_to_bookmarks.rb
@@ -0,0 +1,6 @@
+class AddMovieToBookmarks < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :bookmarks, :movie, null: false, foreign_key: true
+    add_reference :bookmarks, :list, null: false, foreign_key: true
+  end
+end
