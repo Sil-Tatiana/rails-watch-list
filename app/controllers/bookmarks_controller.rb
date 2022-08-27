87cfcec1b9e5c039e2c924d7fd37a42ad4cceea0
Silvia Neves <siltatiana.sn@gmail.com>
2022-08-27 22:50:54 +0100

    adds new features

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -20,7 +20,7 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark.destroy
-    redirect_to lists_path, status: :see_other
+    redirect_to list_path(@bookmark, list), status: :see_other
   end
 
   private
